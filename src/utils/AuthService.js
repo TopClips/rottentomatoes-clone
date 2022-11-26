e415bc845354c0ca8ab86aa208a9c2d53a466b97
TopClips <103296052+TopClips@users.noreply.github.com>
2022-11-26 13:47:59 +1100

    Update AuthService.js

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -6,7 +6,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = 'VbFXkSMcG3PFGeaQFNeNn59wGRhGpm2x';
 const CLIENT_DOMAIN = 'unicoder.auth0.com';
-const REDIRECT = 'https://movicritics.vercel.app/callback';
+const REDIRECT = 'http://localhost:3000';
 const SCOPE = 'openid email profile';
 const AUDIENCE = 'http://rottentomatoes.com';
 
